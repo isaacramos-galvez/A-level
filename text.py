1a1cc1b2f46b30256aec379e40b874bfc9a6f6e8
Isaac <isaac.ramos-galvez@abingdon.org.uk>
2025-09-03 21:30:53 +0100

    initial commit

--- text.py
+++ text.py
@@ -1,2 +1,14 @@
-print("hello")
+# multiplication table game
+
+count = 1
+number = int(input("Enter a number between 1 and 12"))
+
+while number > 12 or number < 1 or number == str(""):
+    number = int(input("Enter a valid number between 1 and 12"))
+
+max = int(input("Please enter the number you wish to times up to"))
+while count <= max:
+    print(f"{count}*{number} is",number * count)
+    count = count + 1
+
 
