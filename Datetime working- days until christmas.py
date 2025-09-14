6514bed946a142aaa87965e29490d696aec0aa22
Isaac <isaac.ramos-galvez@abingdon.org.uk>
2025-09-14 10:04:20 +0100

    Homework task list

--- Datetime working- days until christmas.py
+++ Datetime working- days until christmas.py
@@ -1,10 +1,21 @@
-import datetime
-year = input("What year is your next birthday?")
-month = input("What month is your next birthday in numbers?")
-day = input("What day of the month is your next birthday")
+from datetime import datetime
+def find_birthday():
 
-today = datetime.date.today()
-birthday = datetime.datetime()
-birthday = datetime.datetime(year, month, day)
-time_until_birthday = birthday - today
-print(time_until_birthday.days)
+    year = int(input("What year is your next birthday?"))
+    month = int(input("What month is your next birthday in numbers?"))
+    day = int(input("What day of the month is your next birthday"))
+
+    birthday = datetime(2000 + year, month, day)
+    return birthday
+
+def calculate_days(birthday, today):
+    this_year = datetime(today.year, birthday.month, birthday.day)
+    next_year = datetime(today.year + 1, birthday.month, birthday.day)
+
+    return ((this_year if this_year > today else next_year) - today).days
+   
+today = datetime.today()
+a = find_birthday()
+b = calculate_days(a, today)
+
+print(b)
