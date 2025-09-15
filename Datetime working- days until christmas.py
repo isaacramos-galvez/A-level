8c93691aed6c58b31bec47e08362654701c28069
Isaac <isaac.ramos-galvez@abingdon.org.uk>
2025-09-15 11:23:39 +0100

    Updated Task List Homework.py

--- Datetime working- days until christmas.py
+++ Datetime working- days until christmas.py
@@ -1,21 +1,8 @@
-from datetime import datetime
-def find_birthday():
+task_list = ["hello", "goodbye"]
 
-    year = int(input("What year is your next birthday?"))
-    month = int(input("What month is your next birthday in numbers?"))
-    day = int(input("What day of the month is your next birthday"))
+def complete_task():
+    task_remove = input("What task do you want to remove?")
+    task_list.remove(task_remove)
+    print(task_list)
 
-    birthday = datetime(2000 + year, month, day)
-    return birthday
-
-def calculate_days(birthday, today):
-    this_year = datetime(today.year, birthday.month, birthday.day)
-    next_year = datetime(today.year + 1, birthday.month, birthday.day)
-
-    return ((this_year if this_year > today else next_year) - today).days
-   
-today = datetime.today()
-a = find_birthday()
-b = calculate_days(a, today)
-
-print(b)
+complete_task()
