580772fc73bd9ed753ead4e3b76aa18402e872b4
Isaac <isaac.ramos-galvez@abingdon.org.uk>
2025-09-16 14:39:34 +0100

    changes in lesson

--- Datetime working- days until christmas.py
+++ Datetime working- days until christmas.py
@@ -1,8 +1,22 @@
-task_list = ["hello", "goodbye"]
+from datetime import datetime
+def find_birthday():
 
-def complete_task():
-    task_remove = input("What task do you want to remove?")
-    task_list.remove(task_remove)
-    print(task_list)
+    year = int(input("What year is your next birthday?"))
+    month = int(input("What month is your next birthday in numbers?"))
+    day = int(input("What day of the month is your next birthday"))
 
-complete_task()
+    birthday = datetime(2000 + year, month, day)
+    return birthday
+
+def calculate_days(birthday, today):
+    this_year = datetime(today.year, birthday.month, birthday.day)
+    next_year = datetime(today.year + 1, birthday.month, birthday.day)
+
+    return ((this_year if this_year > today else next_year) - today).days
+   
+today = datetime.today()
+a = find_birthday()
+b = calculate_days(a, today)
+
+print(b)
+
