647eb7fad12328c5f446af1e87b8b2886404b350
Isaac <isaac.ramos-galvez@abingdon.org.uk>
2025-09-24 11:33:24 +0100

    update file testing

--- file testing.py
+++ file testing.py
@@ -1,8 +1,7 @@
 filename = "Computer Science Homework/file_text.txt"
-def return_file():
-    with open("Computer Science Homework/file_text.txt", 'r') as file:
-        text  = file.readlines()
-        print(text)
+def return_file(file_name):
+    with open(filename, 'r') as file:
+        print(file.read())
         if text == "":
             print("this file is empty")
         else:
@@ -11,13 +10,13 @@ def return_file():
 
 def menu():
     file_chosen = input("What file do you want to edit?")
-    open(file_chosen, 'w')
-    if file_chosen == False:
-        print("That file cannot be found")
-    else:
-        input_text = input("What do you want to right to the file?")
-        file_chosen.write(input_text)
-        return_file()
+    with open(file_chosen, 'w') as file:
+        if file_chosen == False:
+            print("That file cannot be found")
+        else:
+            input_text = input("What do you want to write to the file?")
+            file_chosen.write(input_text)
+            return_file()
 
 choice_continue = "yes"
 while choice_continue == "yes":
