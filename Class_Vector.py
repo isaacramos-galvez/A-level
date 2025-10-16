cc227303c96ca35c34e4f3117094d5b95316730c
Isaac <isaac.ramos-galvez@abingdon.org.uk>
2025-10-16 14:03:06 +0100

    Updated_Class_Vector

--- Class_Vector.py
+++ Class_Vector.py
@@ -1,25 +1,28 @@
 import random
 import math
 
-class vector:
+class Vector:
     def __init__(self, i, j):
         self.i = i
         self.j = j
+    
+    def __str__(self):
+        return f"{self.i}i + {self.j}j"
 
     def __add__(self, other):
         add_i = self.i + other.i
         add_j = self.j + other.j
-        return vector(add_i, add_j)
+        return Vector(add_i, add_j)
 
-    def find_magnitude(self):
-        magnitude =  (self.i**2 + self.j**2)**0.5
-        return magnitude
-        
     def __sub__(self, other):
         sub_i = self.i - other.i
         sub_j = self.j - other.j
-        return vector(sub_i, sub_j)
+        return Vector(sub_i, sub_j)
        
+    def magnitude(self):
+        magnitude =  math.sqrt(self.i**2 + self.j**2)
+        return magnitude
+        
     def find_angle(self):
         angle = math.degrees(math.atan(self.j/self.i))
         return angle
@@ -31,9 +34,9 @@ vec2 = (y,random.randint(y,y+10))
 
 print(vec1)
 print(vec2)
-print(vec1.find_magnitude())
-print(vec2.find_magnitude)
-print(vec1.find_angle(self))
-print(vec2.find_angle(self))
+print(vec1.magnitude())
+print(vec2.magnitude())
+print(vec1.find_angle())
+print(vec2.find_angle())
 print(vec1-vec2)
 print(vec1+vec2)
