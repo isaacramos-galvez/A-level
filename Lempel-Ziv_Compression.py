6b06ec61ffbe2f2a40e003f2b075252ffe778441
Isaac <isaac.ramos-galvez@abingdon.org.uk>
2026-03-19 21:52:04 +0000

    commit

--- Lempel-Ziv_Compression.py
+++ Lempel-Ziv_Compression.py
@@ -1,43 +1,33 @@
 #lempel-ziv compression
 message = input("Enter a message with only two different characters e.g. 01101101001010: ")
 codebook = {}
-index = 1
 encrypt_message = ""
 full_char = ""
 edit_message = message
+index = 1
 for char in edit_message:    
     full_char += char
     if char not in codebook:
-        codebook.update({char: index})
+        codebook[full_char] = index
         index += 1
-    else:
-        if full_char not in codebook:
-            codebook.update({full_char: index})
-            index+=1
-            full_char = ""
-    
-        
-    print(char)
-    print(full_char)
-    print(codebook)
+        full_char = ""
 
-full_char = ""
-for new_char in message:
-    full_char = full_char + new_char
-    if full_char in codebook:
-        pass
-    elif full_char not in codebook:
-        full_char = new_char[:len(full_char)-1]
-        if codebook.get(full_char) == None:
-            encrypt_message = encrypt_message + str(codebook.get(char))
-            full_char = ""
-        elif codebook.get(char) == None:
-            encrypt_message = encrypt_message + str(codebook.get(full_char))
-            full_char = ""
-        
-    
-    print(new_char)
-    print(full_char)
-    print(encrypt_message)
-    
-print(encrypt_message)
+total_char = ""
+i = 0
+while i < len(message):
+    total_char += message[i]
+    if i + 1 < len(message):
+        next_char = message[i+1]
+    else: pass
+
+    if next_char != None:
+        combined = total_char + next_char
+    else: combined = total_char
+    if combined not in codebook or next_char == None:
+        add_value = codebook.get(total_char)
+        encrypt_message = encrypt_message + str(add_value)
+        total_char = ""
+    i+=1
+
+print(f"codebook: {codebook}")
+print(f"Encoded: {encrypt_message}")
